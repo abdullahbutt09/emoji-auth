fa6b3f5207bdf51afe1d082c586a31a3b8900171
Abdullah Butt <abdullah.butt.dev.github@gmail.com>
2026-01-17 14:40:40 +0500

    fixed some errors and make the project working

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,14 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home.jsx";
-import Axios from "./pages/Axios.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path='/axios' element={<Axios />} />
       </Routes>
     </Router>
   </StrictMode>
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='text-white font-bold flex items-center justify-center min-h-screen text-3xl bg-gray-700'>
+        <h1>React + Vite 2026 Emoji Authentication</h1>
+    </div>
+  )
+}
+
+export default Home
